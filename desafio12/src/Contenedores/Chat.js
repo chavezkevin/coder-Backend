629a097c3815f6de72ffd90ab077594b9e1400af
kevin.chavez <ami896717@gmail.com>
2022-08-25 17:16:48 -0300

    desafio12

---
+++ desafio12/src/Contenedores/Chat.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose")
+
+class Chat{
+    constructor(collectionName, schema){
+        this.collection = mongoose.model(collectionName, new mongoose.Schema(schema, {timestamps: true}));
+    }
+
+    async save(mensaje){
+        const objetoModel = new this.collection(mensaje);
+
+        try{
+            const res = await objetoModel.save();
+            return res;
+        }
+        
+        catch(err){
+            console.log("Error guardando chat. Code: ", err);
+            return false;
+        }
+    }
+
+    async getAll(){
+        try{
+            const mensajes = await this.collection.find({}, { __v: 0 })
+            return mensajes;
+        } catch(err){
+            console.log("Error guardando chat. Code: ", err);
+            return false;
+        }
+    }
+}
+
+module.exports = Chat;
