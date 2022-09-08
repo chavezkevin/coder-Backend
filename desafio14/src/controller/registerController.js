501729406cd6c92a4fb2014e84426b947de54d9d
kevin.chavez <ami896717@gmail.com>
2022-09-08 18:23:23 -0300

    desafio 14

---
+++ desafio14/src/controller/registerController.js
@@ -0,0 +1,16 @@
+const registerController = async(req, res) =>{
+    
+    res.render('plantillaRegister.ejs')
+}
+
+const registerPostController = async(req, res) =>{
+    req.session.user = req.body.username    
+
+    res.redirect("/api/login")
+}
+
+const registerErrorController = async(req, res) =>{
+    res.render('plantillaRegisterError')
+}
+
+export { registerController, registerPostController, registerErrorController }
